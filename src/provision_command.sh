da8b7b780b2e0ec1b8bf82df0b302b7b439b2744
James Gray <james.gray@amiqus.co>
2026-01-26 12:45:54 +0000

    add yamlfmt

--- src/provision_command.sh
+++ src/provision_command.sh
@@ -83,7 +83,7 @@ install_packages() {
   local cli=(
     ack awscli bat ccal croc curl espanso fd fzf gh git git-branchless git-delta git-extras gnupg gum
     htop httpie jq ncdu lazygit pyenv ripgrep shellcheck starship tfenv
-    the_silver_searcher tig tldr tmux tree watch wget zoxide
+    the_silver_searcher tig tldr tmux tree watch wget yamlfmt zoxide
   )
   for pkg in "${cli[@]}"; do brew_install_formula "$pkg"; done
 
